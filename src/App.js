6b055e64242b72813a48093ca928ea64e5f33518
Khauzen2 <khauzenmavics@gmail.com>
2024-04-24 13:33:07 +0200

    adding header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import ToDoList from './todo.js';
+import Intro from './Components/Intro.js';
 import './App.css';
 
 function App() {
   return (
-   
+    <div>
+      <Intro />
       <ToDoList />
+    </div>
   );
 }
 
---
+++ src/Components/Intro.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Intro extends Component {
+    render() {
+        return (
+            <div className="Intro">
+                <h1>Make up a list of important things to do</h1>
+            </div>
+        );
+    }
+}
+
+export default Intro;
